9c8118e9945dc8c6e52bb5d3abeb07a906dd9c7a
Mounir-mk <mounir.mekoui@gmail.com>
2022-12-23 11:25:47 +0100

    cleaning template, setting up env files and adding tailwind to the project

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,9 @@
-import Home from "./pages/Home";
-
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <Home />
-      <p>coucou</p>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
     </div>
   );
 }
