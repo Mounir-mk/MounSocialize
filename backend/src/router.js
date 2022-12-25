db8f5a54f9c041a61e1ad8c626779858107d2f50
Mounir-mk <mounir.mekoui@gmail.com>
2022-12-25 18:15:59 +0100

    setting up dummy routes for likes and comments (add edit delete)

--- backend/src/router.js
+++ backend/src/router.js
@@ -26,9 +26,28 @@ router.get("/users/:id", userController.read);
 
 // routes concerning posts
 router.get("/posts", postController.browse);
-router.get("/posts/:id", postController.read);
 router.get("/users/:id/posts", postController.browseByUser);
+router.get("/users/:id/posts/:postId", postController.read);
 router.get("/users/:id/posts/:postId/comments", postController.browseComments);
 router.get("/users/:id/posts/:postId/likes", postController.browseLikes);
+router.post("/users/:id/posts", postController.add);
+router.put("/users/:id/posts/:postId", postController.edit);
+router.delete("/users/:id/posts/:postId", postController.delete);
+
+// routes concerning comments
+router.post("/users/:id/posts/:postId/comments", postController.addComment);
+router.put(
+  "/users/:id/posts/:postId/comments/:commentId",
+  postController.editComment
+);
+router.delete(
+  "/users/:id/posts/:postId/comments/:commentId",
+  postController.deleteComment
+);
+
+// routes concerning likes
+router.post("/users/:id/posts/:postId/likes", postController.addLike);
+router.put("/users/:id/posts/:postId/likes", postController.editLike);
+router.delete("/users/:id/posts/:postId/likes", postController.deleteLike);
 
 module.exports = router;
